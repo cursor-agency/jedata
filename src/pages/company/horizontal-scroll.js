69c938d5277c20c4e4667d86be7ea8202932ca31
Misha Zubkov <m.zubkov@juicedev.org>
2023-11-20 14:56:12 +0300

    chore: поменял точку старта анимации текста

--- src/pages/company/horizontal-scroll.js
+++ src/pages/company/horizontal-scroll.js
@@ -60,7 +60,7 @@ animatedNodes.forEach((node, index) => {
     scrollTrigger: {
       containerAnimation: timeline,
       trigger: `[animate="${index+1}"]`,
-      start: 'left center',
+      start: 'left left',
       markers: true,
     },
   })
