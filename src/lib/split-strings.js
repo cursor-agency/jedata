cab0c63f6882a1434b20f6880f29249d84d511c4
Misha Zubkov <m.zubkov@juicedev.org>
2023-11-20 05:29:44 +0300

    fix(text-animation): querySelector -> querySelectorAll

--- src/lib/split-strings.js
+++ src/lib/split-strings.js
@@ -1,5 +1,5 @@
 export const splitStrings = (selector) => {
-  const elements = document.querySelector(selector);
+  const elements = document.querySelectorAll(selector);
   elements?.forEach(node => {
     const words = node.textContent.split(' ');
     words?.forEach(word => {
