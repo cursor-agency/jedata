b9d1c34cb91ea3b424ffc83159147ebd5784812f
Zu6koff <47788849+Zu6koff@users.noreply.github.com>
2023-11-20 02:22:28 +0000

    Deploying to gh-pages from @ cursor-agency/jedata@cd849e27b43a85dfb01e5d5c86d321623b664a50 🚀

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-(()=>{var t=e=>{document.querySelector(e).forEach(o=>{o.textContent.split(" ").forEach(n=>{console.log(n)})})};window.addEventListener("DOMContentLoaded",()=>{t("animated-text")});})();
+(()=>{var t=e=>{document.querySelector(e).forEach(o=>{o.textContent.split(" ").forEach(n=>{console.log(n)})})};window.addEventListener("DOMContentLoaded",()=>{t("[animate]")});})();
